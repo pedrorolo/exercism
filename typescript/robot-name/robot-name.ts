d1a4380b28ed62c276bf47d4681eaa58ac11d016
Pedro Rolo <pedrorolo@gmail.com>
2020-11-24 14:29:20 +0000

    ts robot name

---
+++ typescript/robot-name/robot-name.ts
@@ -0,0 +1,54 @@
+import { getPriority } from "os"
+import { cachedDataVersionTag } from "v8"
+
+type RobotsExistenceDict = {
+    [key: string]: boolean
+}
+
+const robots: RobotsExistenceDict = {}
+
+
+const randomBetween = (x: number, y: number) => {
+    const domainSize = y - x
+    const randWithout1 = (): number => {
+        const rand = Math.random()
+        if (rand != 1)
+            return rand
+        return randWithout1()
+    }
+    return x + Math.floor((domainSize + 1) * randWithout1())
+}
+
+
+const genRandUpcaseLetter = () => {
+    const code = (letter: string) => letter.codePointAt(0) as number
+    const a = code("A")
+    const z = code("Z")
+    return String.fromCodePoint(randomBetween(a, z))
+}
+
+
+const genRandomNumDigit = () => {
+    return "" + randomBetween(0, 9)
+}
+
+const genRobotName = (): string => {
+    const d = () => genRandomNumDigit()
+    const l = () => genRandUpcaseLetter()
+    const candidate = l() + l() + d() + d() + d()
+    if (!robots[candidate]) {
+        robots[candidate] = true
+        return candidate;
+    }
+    return genRobotName()
+}
+
+export default class RobotName {
+    name: string
+    constructor() {
+        this.name = genRobotName()
+    }
+    resetName(): void {
+        this.name = genRobotName()
+    }
+}
