30386d73688580939831c58ad2dd42d917ca8f8c
Pedro Rolo <pedro@hypofriend.de>
2024-05-11 22:12:00 +0100

    anagram

---
+++ rust/anagram/src/lib.rs
@@ -0,0 +1,15 @@
+use std::collections::HashSet;
+use std;
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+  let word_chars= HashSet::from_iter(word.chars());
+  possible_anagrams.into_iter()
+                   .filter(|w| has_chars(&word_chars, w))
+                    .copied()
+                    .collect()
+}
+
+fn has_chars(chars: &HashSet<char>, word: &str) -> bool{
+  HashSet::from_iter(word.chars()) == *chars
+}
+
